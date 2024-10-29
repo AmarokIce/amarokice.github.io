7e6c30f27c98336aa592d796bb381f44ba9ac86f
AmarokIce <snowlyicewolf@outlook.com>
2024-10-30 00:01:47 +0800

    🐺🔧

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1730217378',resources: [
+    cacheName: 'chirpy-1730217688',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
